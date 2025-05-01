48bcb69ea10a77f803bb581da52cce3d0e2cb0d2
sakagami-t <sakagamit@nihonsoft.co.jp>
2025-05-01 16:33:07 +0900

    Replace ES6 import/export statements with CommonJS require/module.exports statements in `faceDetection.js`
    * **faceDetection.js**
      - Replace ES6 import statement with CommonJS require statement
      - Replace ES6 export statement with CommonJS module.exports statement
    
    * **test.html**
      - Add type="module" attribute to the script tag for `faceDetection.js`
      - Add type="module" attribute to the script tag for `face-api.js`

--- faceDetection.js
+++ faceDetection.js
@@ -1,4 +1,4 @@
-import * as faceapi from 'face-api.js';
+const faceapi = require('face-api.js');
 
 async function loadModels() {
     await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
@@ -27,4 +27,4 @@ function drawDetections(video, detections) {
     document.body.append(canvas);
 }
 
-export { loadModels, detectFaces, drawDetections };
+module.exports = { loadModels, detectFaces, drawDetections };
