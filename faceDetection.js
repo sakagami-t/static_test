9e0ce2b623de3f1ed8add5755341ceefcb88abda
sakagami-t <sakagamit@nihonsoft.co.jp>
2025-05-01 16:28:26 +0900

    Add face detection functionality using Face API
    * **faceDetection.js**
      - Import Face API library
      - Load Face API models
      - Create function to detect faces in video feed
      - Create function to draw red rectangles around detected faces
      - Export face detection and drawing functions
    
    * **test.html**
      - Add script tag to include Face API library
      - Add script tag to include `faceDetection.js`
      - Call face detection function after video feed starts
      - Add event listener to detect and draw faces at intervals

---
+++ faceDetection.js
@@ -0,0 +1,30 @@
+import * as faceapi from 'face-api.js';
+
+async function loadModels() {
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+}
+
+async function detectFaces(video) {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+    return detections;
+}
+
+function drawDetections(video, detections) {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.width;
+    canvas.height = video.height;
+    const context = canvas.getContext('2d');
+    context.strokeStyle = 'red';
+    context.lineWidth = 2;
+
+    detections.forEach(detection => {
+        const { x, y, width, height } = detection.box;
+        context.strokeRect(x, y, width, height);
+    });
+
+    document.body.append(canvas);
+}
+
+export { loadModels, detectFaces, drawDetections };
